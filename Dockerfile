fae239ddbb567e6aa17e1998375c4fa9214fd216
asbiamrullah22 <asbi.amrullah25@gmail.com>
2021-04-28 14:13:57 +0700

    no message

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM ubuntu:18.04
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN echo "resolvconf resolvconf/linkify-resolvconf boolean false" | debconf-set-selections
+
+#Install Depedensi
+RUN apt-get update && apt-get install -y bind9 bind9utils ssh netcat-openbsd sudo libidn11 libpcre3 libgmp10 libexpat1 libstdc++6 libperl5.26 libaio1 resolvconf unzip pax sysstat sqlite3 dnsutils iputils-ping w3m gnupg less lsb-release rsyslog net-tools vim tzdata wget iproute2 locales curl
+
+# Configurasi Time
+RUN echo "tzdata tzdata/Areas select Asia\ntzdata tzdata/Zones/Asia select Jakarta" > /tmp/tz ; debconf-set-selections /tmp/tz; rm /etc/localtime /etc/timezone; dpkg-reconfigure -f noninteractive tzdata
+
+VOLUME ["/opt/zimbra"]
+
+RUN mv /etc/init.d/rsyslog /tmp/
+RUN curl -k https://raw.githubusercontent.com/imanudin11/zimbra-docker/master/rsyslog > /etc/init.d/rsyslog
+RUN chmod +x /etc/init.d/rsyslog
+
+RUN (crontab -l 2>/dev/null; echo "1 * * * * /etc/init.d/rsyslog restart > /dev/null 2>&1") | crontab -
+
+RUN /etc/init.d/rsyslog restart
+
+COPY start.sh .
+
+CMD [ "/bin/bash", "start.sh", "-d" ]
---
+++ start.sh
@@ -0,0 +1,41 @@
+echo 'export LC_ALL="en_US.UTF-8"' >> /root/.bashrc
+locale-gen en_US.UTF-8
+
+echo 'cat /etc/resolv.conf > /tmp/resolv.ori' > /services.sh
+echo 'echo "nameserver 127.0.0.1" > /tmp/resolv.add' >> /services.sh
+echo 'cat /tmp/resolv.add /tmp/resolv.ori > /etc/resolv.conf' >> /services.sh
+echo '/etc/init.d/bind9 restart' >> /services.sh
+echo '/etc/init.d/rsyslog restart' >> /services.sh
+#RUN echo '/etc/init.d/zimbra restart' >> /services.sh
+chmod +x /services.sh
+
+/services.sh
+
+curl -k https://raw.githubusercontent.com/asbiamrullah22/zimbra/master/dns-auto.sh > /srv/dns-auto.sh
+chmod +x /srv/dns-auto.sh
+
+/srv/dns-auto.sh
+
+## Install Zimbra
+echo "Downloading Zimbra OpenSource 8.8.15"
+wget -O /opt/zcs-8.8.15_GA_3869.UBUNTU18_64.20190917004220.tgz https://files.zimbra.com/downloads/8.8.15_GA/zcs-8.8.15_GA_3869.UBUNTU18_64.20190917004220.tgz
+echo "Extracting files from the archive"
+tar -xzvf /opt/zcs-8.8.15_GA_3869.UBUNTU18_64.20190917004220.tgz -C /opt/
+
+echo "Update package cache"
+apt update
+
+echo "Installing Zimbra Collaboration just the Software"
+cd /opt/zcs-8.8.15_GA_3869.UBUNTU18_64.20190917004220 && ./install.sh
+
+su - zimbra -c 'zmcontrol restart'
+echo "You can access now to your Zimbra Collaboration Server"
+
+
+if [[ $1 == "-d" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+  /bin/bash
+fi
